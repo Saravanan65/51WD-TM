49d0e11e72ed8e97095476b048b1c03d91d3a2c7
Saravanan65 <saravananbba@gmail.com>
2023-12-27 16:50:23 +0530

    midifided

--- Day-3/Day-3-task/XHM-request/script.js
+++ Day-3/Day-3-task/XHM-request/script.js
@@ -10,11 +10,11 @@ xhr.onload = function () {
   if (xhr.status === 200) {
 
     let details = JSON.parse(xhr.responseText);
-    // console.log(details)
+     console.log(details)
     for (let i = 0; i < details.length; i++) {
 
       console.log("");
-      console.log("Flag", "==>", details[i].flag)
+      console.log("Flag", "==>", details[i].flags.png)
       console.log("Name", "==>", details[i].name.common)
       console.log("Region", "==>", details[i].region)
       console.log("Sub region", "==>", details[i].subregion)
