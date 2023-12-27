96f11211dcf19a15e770c8c4b0dfb34eaa060a65
Saravanan65 <saravananbba@gmail.com>
2023-12-27 16:43:47 +0530

    added

---
+++ Day-2/js/script.js
@@ -0,0 +1,12 @@
+ let obj = {
+    name: "saravanan",
+    Place: "Pondicherry",
+    Work : "Support",
+   Graduation:{
+    UG: "BBA General",
+    PG:"MBA HRM"
+   }
+ }
+ for (let key in obj){
+    console.log(key, "==>", obj[key]);
+ }1
---
+++ Day-3/Day-3-task/XHM-request/script.js
@@ -0,0 +1,27 @@
+let xhr = new XMLHttpRequest();
+let method = "GET";
+let url = "https://restcountries.com/v3.1/all";
+
+xhr.open(method, url);
+xhr.send();
+
+xhr.onload = function () {
+
+  if (xhr.status === 200) {
+
+    let details = JSON.parse(xhr.responseText);
+    // console.log(details)
+    for (let i = 0; i < details.length; i++) {
+
+      console.log("");
+      console.log("Flag", "==>", details[i].flag)
+      console.log("Name", "==>", details[i].name.common)
+      console.log("Region", "==>", details[i].region)
+      console.log("Sub region", "==>", details[i].subregion)
+      console.log("Total population", "==>", details[i].population)
+    }
+  } else {
+
+    console.log("Request failed", xhr.status);
+  }
+}
